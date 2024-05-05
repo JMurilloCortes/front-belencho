60637240d57de79c67b2958f6ce4296b7dd02db5
JMurilloCortes <jealmuco@gmail.com>
2024-05-05 03:59:31 -0500

    Consumiendo la informacion de la DB desde el Back

--- src/Components/Detail/Detail.jsx
+++ src/Components/Detail/Detail.jsx
@@ -31,22 +31,6 @@ const Detail = () => {
     return <div>Cargando...</div>;
   }
 
-  // Generar las 5 estrellas, colorear las primeras 'rating' estrellas
-  // const ratingStars = [...Array(5)].map((_, index) => (
-  //   <svg
-  //     key={index}
-  //     fill={index < Math.round(item.rating?.rate) ? "currentColor" : "none"}
-  //     stroke="currentColor"
-  //     strokeLinecap="round"
-  //     strokeLinejoin="round"
-  //     strokeWidth="2"
-  //     className="w-4 h-4 text-green-500"
-  //     viewBox="0 0 24 24"
-  //   >
-  //     <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
-  //   </svg>
-  // ));
-
   return (
     <section className="text-gray-600 body-font overflow-hidden">
       <div className="container px-5 py-24 mx-auto">
@@ -54,8 +38,11 @@ const Detail = () => {
           <img
             alt="ecommerce"
             className="lg:w-1/2 w-full lg:h-auto h-64 object-cover object-center rounded"
-            src={item.imagenes[1]}
+            src={
+              item.imagenes && item.imagenes.length > 0 ? item.imagenes[0] : ""
+            }
           ></img>
+
           <div className="lg:w-1/2 w-full lg:pl-10 lg:py-6 mt-6 lg:mt-0">
             <h2 className="text-sm title-font text-gray-500 tracking-widest">
               {item.categoria}
@@ -64,9 +51,6 @@ const Detail = () => {
               {item.nombre}
             </h1>
             <div className="flex mb-4">
-              <span className="flex items-center">
-                {/* <span className="flex text-gray-600 ml-3">{ratingStars}</span> */}
-              </span>
               <span className="flex ml-3 pl-3 py-2 border-l-2 border-gray-200 space-x-2s">
                 <a className="text-gray-500">
                   <svg
@@ -108,35 +92,10 @@ const Detail = () => {
             </div>
             <p className="leading-relaxed">{item.descripcion}</p>
             <div className="flex mt-6 items-center pb-5 border-b-2 border-gray-100 mb-5">
-              <div className="flex">
-                <span className="mr-3">Color</span>
-                <button className="border-2 border-gray-300 rounded-full w-6 h-6 focus:outline-none"></button>
-                <button className="border-2 border-gray-300 ml-1 bg-gray-700 rounded-full w-6 h-6 focus:outline-none"></button>
-                <button className="border-2 border-gray-300 ml-1 bg-green-500 rounded-full w-6 h-6 focus:outline-none"></button>
-              </div>
+              
               <div className="flex ml-6 items-center">
-                <span className="mr-3">Size</span>
-                <div className="relative">
-                  <select className="rounded border appearance-none border-gray-300 py-2 focus:outline-none focus:ring-2 focus:ring-green-200 focus:border-green-500 text-base pl-3 pr-10">
-                    <option>SM</option>
-                    <option>M</option>
-                    <option>L</option>
-                    <option>XL</option>
-                  </select>
-                  <span className="absolute right-0 top-0 h-full w-10 text-center text-gray-600 pointer-events-none flex items-center justify-center">
-                    <svg
-                      fill="none"
-                      stroke="currentColor"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      className="w-4 h-4"
-                      viewBox="0 0 24 24"
-                    >
-                      <path d="M6 9l6 6 6-6"></path>
-                    </svg>
-                  </span>
-                </div>
+              
+                <div className="relative"></div>
               </div>
             </div>
             <div className="flex">
